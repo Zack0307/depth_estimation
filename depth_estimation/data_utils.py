f6011e2c8fe990e191b49c7ab83e0499870ab9ca
Zack930307 <box930307@gmail.com>
2025-07-02 11:45:56 +0800

    delete useless

--- depth_estimation/data_utils.py
+++ depth_estimation/data_utils.py
@@ -7,12 +7,8 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from torchvision import models, transforms
 from PIL import Image as PILImage
-from torchvision.models.segmentation import (deeplabv3_resnet50,  DeepLabV3_ResNet50_Weights)
 
 
-def DeepLabv3(weights):
-    model = deeplabv3_resnet50(weights = weights)
-    model.eval()
    
         
         
